ca999fcae9a6db1ce0f2f065342541f496db93da
alex9508 <he352125@uaeh.edu.mx>
2021-04-06 16:12:58 -0500

    Game laptop

--- game/game.js
+++ game/game.js
@@ -8,11 +8,12 @@ class Juego {
 		constructor() {
 				this.inicializar()
 				this.generarSecuencia()
+				this.siguienteNivel()
 		}
 
     inicializar() {
 				btnEmpezar.classList.add('hide')
-				this.nivel = 1
+				this.nivel = 7 
 				this.colores = {
 						celeste,
 						violeta,
@@ -24,9 +25,43 @@ class Juego {
 		generarSecuencia(){
 				this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4))
 		}
+
+		siguienteNivel(){
+				this.iluminarSecuencia()
+		}
+
+		transformarNumeroAColor(num){
+				switch (num){
+						case 0:
+								return 'celeste'
+						case 1:
+								return 'violeta'
+						case 2:
+								return 'naranja'
+						case 3:
+								return 'verde'
+
+				}
+		}
+
+		iluminarSecuencia(){
+				for(let i = 0; i < this.nivel; i++){
+						let color = this.transformarNumeroAColor(this.secuencia[i])
+						setTimeout(() => this.iluminarColor(color), 1000 * i)
+				} 
+		}
+
+		iluminarColor(color){
+				this.colores[color].classList.add('light')
+				setTimeout(() => this.apagarColor(color), 350)
+		}
+
+		apagarColor(color){
+				this.colores[color].classList.remove('light')
+		}
 }
 
 function empezarJuego() {
-		var juego = new Juego()
+		window.juego = new Juego()
 }
 
